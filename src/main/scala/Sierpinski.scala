959863ba16c21d1221fb5e7a6f979ac8e3da5f38
Louis J B <chillipower.uk@gmail.com>
2024-10-04 22:20:06 -0400

    Add double buffered screen rendering and a random colour option

--- src/main/scala/Sierpinski.scala
+++ src/main/scala/Sierpinski.scala
@@ -1,20 +1,28 @@
-import java.awt.{Color, Graphics2D, Point, geom}
-
+import Math._
 import scala.swing.Swing._
 import scala.swing.event._
 import scala.swing.{Frame, MainFrame, Panel, SimpleSwingApplication}
+import javax.swing.Timer
+import java.awt.{Color, Graphics2D, Point, geom}
 import java.awt.{event => jae}
 import java.awt.Polygon
 import java.awt.RenderingHints
-import javax.swing.Timer
+import java.awt.image.BufferedImage
+import scala.util.Random
+
 
 object Sierpinski extends SimpleSwingApplication {
-  private val defaultSize = 600
-  private val maxDepth = 10
+  import GraphicUtils._
+  private val defaultSize = 800
+  private val maxDepth = 7
   private val borderSize = 5
-  private val delay = 1000
+  private val delayMs = 500
+  private var randomColor = false
   private val startTime = System.currentTimeMillis() // just so it starts nicely at 0
-  private def currMaxDepth = (System.currentTimeMillis() - startTime) / 1000 % maxDepth
+  private def currMaxDepth = {
+    val x = (System.currentTimeMillis() - startTime) / 1000 % (maxDepth * 2)
+    min(x , 2 * maxDepth - x)
+  }
 
   lazy val ui: Panel = new Panel with SettableFgColor {
     background = Color.yellow
@@ -30,12 +38,16 @@ object Sierpinski extends SimpleSwingApplication {
 
     listenTo(keys)
     reactions += {
-      case KeyPressed(_, Key.C, _, _) =>
-        new ColorChooserFrame(this)
+      case KeyPressed(_, key, _, _) => key match {
+        case Key.C =>
+          new ColorChooserFrame(this)
+        case Key.R =>
+          randomColor = !randomColor
+      }
       case KeyReleased(_, Key.Space, _, _) =>
     }
     
-    val timer = new Timer(delay, new jae.ActionListener() {
+    val timer = new Timer(delayMs, new jae.ActionListener() {
       def actionPerformed(e: jae.ActionEvent): Unit = {
         repaint()
       }
@@ -44,30 +56,38 @@ object Sierpinski extends SimpleSwingApplication {
 
     override def paintComponent(g: Graphics2D): Unit = {
       super.paintComponent(g)
-      val g2d = g.asInstanceOf[Graphics2D]
+      val buffImg = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB)
+      val g2d = buffImg.createGraphics()
       g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
 
-      g.setColor(fgColor)
-      
+      g2d.setColor(background)
+      g2d.fillRect(0, 0, size.width, size.height)
       def tri(x: Int, y: Int, s: Int, depth: Int = 0): Unit = {
         val midX = x + s / 2
         val midY = y + s / 2
+        if (depth == 0) {
+          g2d.setColor(fgColor)
+          g2d.fillPolygon(
+            mkPoly(List(
+              (midX, y),
+              (x, y+s),
+              (x+s, y+s)
+            ))
+          )
+        }
+        if (randomColor)
+          g2d.setColor(rndColor())
+        else
+          g2d.setColor(Color.WHITE)
+        g2d.fillPolygon(
+          mkPoly(List(
+            (x+s/4, midY),
+            (x + 3*s/4, midY),
+            (midX, y+s)
+          ))
+        )
         
-        g2d.setColor(fgColor)
-        val triangle = new Polygon()
-        triangle.addPoint(midX, y)
-        triangle.addPoint(x, y+s)
-        triangle.addPoint(x+s, y+s)
-        g2d.fillPolygon(triangle)
-   
-        g2d.setColor(Color.white)
-        val triangleInner = new Polygon()
-        triangleInner.addPoint(x+s/4, midY)
-        triangleInner.addPoint(x + 3*s/4, midY)
-        triangleInner.addPoint(midX, y+s)
-        g2d.fillPolygon(triangleInner)
-        
-        if (depth < currMaxDepth && s > 10) {
+        if (depth < currMaxDepth && s > 5) {
           tri(x+s/4, y, s/2, depth + 1)
           tri(x, midY, s/2, depth + 1)
           tri(midX, midY, s/2, depth + 1)
@@ -75,6 +95,10 @@ object Sierpinski extends SimpleSwingApplication {
       }
       val currentSize = Math.min(ui.size.width, ui.size.height)
       tri(borderSize, borderSize, currentSize - 2 * borderSize)
+
+      val g2dr = g.asInstanceOf[Graphics2D]
+      g2dr.drawImage(buffImg, 0, 0, null)
+      g2d.dispose()
     }
   }
 
@@ -84,8 +108,16 @@ object Sierpinski extends SimpleSwingApplication {
     pack().centerOnScreen()
     open()
   }
+
+  def mkPoly(points : List[(Int, Int)]) =
+    points.foldRight(new Polygon())((p, pgn) => { pgn.addPoint.tupled(p); pgn })
 }
 
 trait SettableFgColor {
-  def setFgColor(c : Color): Unit
+  def setFgColor(c: Color): Unit
+}
+
+object GraphicUtils {
+  val rand = new Random()
+  def rndColor() = new Color(rand.nextFloat, rand.nextFloat, rand.nextFloat)
 }
